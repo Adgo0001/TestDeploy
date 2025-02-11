e51a63753f72fc25c3708ccb7af122bebbdf5eca
Adrian Goldbech <adgo0001@stud.kea.dk>
2025-02-11 14:00:40 +0100

    Initial commit

---
+++ src/test/java/dk/kea/testdeploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.testdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
